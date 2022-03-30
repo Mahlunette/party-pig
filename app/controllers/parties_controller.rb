c76a21f7a9aabbb7db44929b8d238f0a83ba8453
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-03-30 10:57:39 +0200

    Party controller added

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,3 +1,25 @@
 class PartiesController < ApplicationController
-  def 
+  def show
+    @party = Party.find(params[:id])
+  end
+
+  def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.new(party_params)
+    if @party.save
+      redirect_to party_path(@party)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:name)
+  end
+
 end
