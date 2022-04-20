65a453ed0755a5ab1988887c2983ecef24a2abce
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-04-20 11:23:19 +0200

    add an eventListener (keyUp)

--- app/javascript/components/AddBucketForm.jsx
+++ app/javascript/components/AddBucketForm.jsx
@@ -1,14 +1,29 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 
 export const AddBucketForm = () => {
   const [value, setValue] = useState("")
+  const handleSubmit = (bucketName) => {
+    
+
+  }
+  const handleKeyUp = event => {
+    if (event.key === 'Enter') {
+      handleSubmit(value);
+    }
+  }
   return (
-    <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+    <input type="text"
+      value={value}
+      onChange={event => setValue(event.target.value)}
+      onKeyUp={handleKeyUp }
+    />
   )
 }
 
-// we need to get the value from the input element(through the eventhandler)
-
 //we need to save the value in the state through the setValue
+// we need to send the params(url) to the controller
+// controller saves the bucket to the db
+// condition: if successful, send a message to the view and if not, send another message.
+// if updated: change the state, if not, nothing happens.
 //
